dae3f8c960afc9b585e241587af71bc634e2649e
Aldi L William <aldyy1988@gmail.com>
2022-10-25 11:37:52 +0700

    Update Terbaru

--- src/pages/Dashboard/BursaMobil.vue
+++ src/pages/Dashboard/BursaMobil.vue
@@ -268,6 +268,7 @@ const favorite = (id:any) =>{
       <div v-for="(product,i) in products" :key="i+'products'"
         class="col-span-12 sm:col-span-4 md:col-span-4 lg:col-span-4 xl:col-span-4 2xl:col-span-4 z-10 bg-white">
         <CardComponentMobil :product="product" @add-fav="favorite" />
+        <CardComponentMobilSmartPhone :product="product" @add-fav="favorite" />
       </div>
       <div v-if="products.length < 1" class="col-span-12">
         data belum ada
