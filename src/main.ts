19c1821b316182fe16bc61d5ae689319f84c558a
mas-yan <mrey5597@gmail.com>
2022-11-02 13:37:16 +0700

    tes

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import VueEasyLightbox from 'vue-easy-lightbox'
 import VueTippy from 'vue-tippy'
 import 'tippy.js/dist/tippy.css' // optional
 import Echo from "laravel-echo";
-import Pusher from "pusher-js"; //pusher
+import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 const app = createApp(App)
