d1c21cf730052ea614c44c3e9fafc925247f5c25
Aldi L William <aldyy1988@gmail.com>
2022-11-21 08:26:19 +0700

    Update

--- src/components/CardComponentMobil.vue
+++ src/components/CardComponentMobil.vue
@@ -79,11 +79,15 @@ onMounted(() => {
     cluster: "ap1",
   });
 
+  let countPeople = ref(0);
+  let countBidding = ref(0);
   echo.channel('bidding')
     .listen('BiddingEvent', (e) => {
       // console.log(e.bidding.price_winner);
       if (products.product.id == e.bidding.id) {
         products.product.price_winner = e.bidding.price_winner
+        countPeople.value = e.bidding.CountPeople
+        countBidding.value = e.bidding.countBidding
       }
     });
 })
@@ -102,11 +106,11 @@ onMounted(() => {
     <div class="grid grid-cols-12 my-2 gap-2">
       <div class="col-span-4 bg-abu_abu relative flex px-2 py-1 rounded items-center">
         <img :src="image_hammer" alt="hammer" class="w-6 h-6" />
-        <div class="text-white mx-2 right-0 relative">1000</div>
+        <div class="text-white mx-2 right-0 relative">{{ countBidding }}</div>
       </div>
       <div class="col-span-4 bg-abu_abu relative flex px-2 py-1 rounded items-center">
         <img :src="image_users" alt="users" class="w-6 h-6" />
-        <div class="text-white mx-2">1000</div>
+        <div class="text-white mx-2">{{ countPeople }}</div>
       </div>
       <div class="col-span-4">
         <h1 class="text-sm text-center bg-blue-500 text-white rounded-lg px-2 py-1 w-20 float-right">TAV</h1>
