f0da1df049f950828465f0c4fcec6116b2fd9d0e
Aldi L William <aldyy1988@gmail.com>
2022-11-23 20:32:25 +0700

    Update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,7 @@
     import { useRouter } from 'vue-router';
     import image from '../assets/images/splashscreen.png';
     import logo from '../assets/images/logo.png';
+    import logo_biru from '../assets/images/logo_biru.png';
     import mata_password from '../assets/images/mata_password.png';
     import star from '../assets/images/star_splash_screen.png';
     import LoginService from '../services/UserService';
@@ -90,7 +91,7 @@
   </div> 
   
   <div class="w-full h-full bg-secondary py-20 absolute" :class="isAnimated ? 'hidden':''">
-    <img :src="logo" alt="tav logo" class="w-64 mx-auto my-4" preload/>
+    <img :src="logo_biru" alt="tav logo" class="w-40 mx-auto" preload/>
     <pre class="text-blue-500 text-3xl tracking-normal sm:tracking-widest md:tracking-widest lg:tracking-widest xl:tracking-widest 2xl:tracking-widest text-center my-4">LELANG MENANG</pre>
     <img :src="star" alt="tav star" class="w-20 mx-auto my-4" preload/>
     <div id="preload">
@@ -100,14 +101,14 @@
     
     <p class="text-center my-4 font-bold">Berburu mobil bekas Murah & Berkualitas</p>
   </div>
-  <div class="hidden sm:block w-full h-full flex flex-col justify-center layer -top-10 bg-slate-300" :class="isAbsolute ? 'absolute':''">  
+  <div class="hidden sm:block w-full h-full flex flex-col justify-center layer pt-12 bg-slate-300" :class="isAbsolute ? 'absolute':''">  
      <div class="ml-auto mr-64 w-11/12 sm:w-3/12 md:w-3/12 lg:w-3/12 xl:w-3/12 2xl:w-3/12 p-8 rounded-xl">
           <div class="w-full flex justify-center z-10" v-show="isAnimated2">
               <div class="flex">
                 <img :src="logo" class="w-40">
               </div>
           </div> 
-          <h1 class="text-center text-3xl font-bold mb-2">SELAMAT DATANG</h1>
+          <h1 class="text-center text-xl font-bold mb-2">SELAMAT DATANG</h1>
           <p class="text-center mb-6 text-lg">Platform LELANG MENANG Mobil Bekas Berkualitas</p>
           <div class="w-full mx-auto">
               <p class="text-left">Username</p>
@@ -118,7 +119,7 @@
             <p class="text-left">Password</p>
             <input placeholder="Masukan Password" class="bg-white px-4 py-2 rounded border border-black w-full" v-model="dataLogin.password" :type="passwordFieldType"/>
             <div class="relative" v-show="isAnimated2">
-              <button @click="switchVisibility()" class="absolute -top-[29px] right-4"><img :src="mata_password" class="w-6 h-4"></button>
+              <button @click="switchVisibility()" class="absolute -top-[10px] right-4"><img :src="mata_password" class="w-6 h-4"></button>
             </div>
             
           </div>
@@ -144,34 +145,34 @@
      <div class="mx-auto sm:ml-auto sm:mr-64 w-11/12 sm:w-3/12 md:w-3/12 lg:w-3/12 xl:w-3/12 2xl:w-3/12 p-8 rounded-xl">
           <div class="w-full flex justify-center z-10" v-show="isAnimated2">
               <div class="flex">
-                <img :src="logo" class="w-40">
+                <img :src="logo" class="w-32">
               </div>
           </div> 
           <h1 class="text-center text-3xl font-bold mb-2">SELAMAT DATANG</h1>
-          <p class="text-center mb-6 text-lg">Platform LELANG MENANG Mobil Bekas Berkualitas</p>
+          <p class="text-center mb-2 text-lg">Platform LELANG MENANG Mobil Bekas Berkualitas</p>
           <div class="w-full mx-auto">
               <p class="text-left">Username</p>
-              <input placeholder="Masukan No. WhatsApp" class="bg-white px-4 py-2 rounded border border-black w-full" v-model="dataLogin.no_whatsapp" type="number"/>
+              <input placeholder="Masukan No. WhatsApp" class="bg-white px-2 py-1 rounded border border-black w-full" v-model="dataLogin.no_whatsapp" type="number"/>
           </div>
      
           <div class="w-full mx-auto my-4">
             <p class="text-left">Password</p>
-            <input placeholder="Masukan Password" class="bg-white px-4 py-2 rounded border border-black w-full" v-model="dataLogin.password" :type="passwordFieldType"/>
+            <input placeholder="Masukan Password" class="bg-white px-2 py-1 rounded border border-black w-full" v-model="dataLogin.password" :type="passwordFieldType"/>
             <div class="relative" v-show="isAnimated2">
-              <button @click="switchVisibility()" class="absolute -top-[29px] right-4"><img :src="mata_password" class="w-6 h-4"></button>
+              <button @click="switchVisibility()" class="absolute -top-[25px] right-4"><img :src="mata_password" class="w-6 h-4"></button>
             </div>
             
           </div>
      
-          <div class="w-full mx-auto mb-4 flex flex-col sm:flex-row justify-between">
+          <div class="w-full mx-auto mb-2 flex flex-col sm:flex-row justify-between">
             <div>
               <input type="checkbox"/>
               <span class="mx-2">Ingat Password Saya</span>
             </div>   
           </div>
-          <button class="bg-blue-500 text-white px-4 py-2 w-full mx-auto rounded" @click="logintodashboard()">Masuk</button>
+          <button class="bg-blue-500 text-white px-2 py-1 w-full mx-auto rounded" @click="logintodashboard()">Masuk</button>
           <div class="flex justify-center mt-1">
-            <a href="#" class="text-blue-400 hover:text-blue-500" @click="showModal = true">Lupa Password ?</a>
+            <a href="#" class="text-white" @click="showModal = true">Lupa Password ?</a>
           </div>
           
           <!-- <div class="text-center">Belum memiliki Akun ? Silahkan <a href="https://docs.google.com/forms/d/e/1FAIpQLSeUmuz7mHM__fyJl9WazOxum0_NLmT4-IPHWFqmroCQe2CWdw/viewform" class="hover:text-blue-500 text-blue-400">daftar disini</a></div>
