32c7899fde505511c2946c872d2b02a863a1fe28
Aldi L William <aldyy1988@gmail.com>
2022-11-24 16:34:10 +0700

    Update

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -26,7 +26,6 @@
       })
   }
 
-let priceWinner = ref(0);
 onMounted(() => {
   // http.get(`https://admin.tavmobil.id/api/lelang/daftar-lelang/${products.product.id}`).then(res => {
   //   console.log(res)
@@ -41,7 +40,7 @@ onMounted(() => {
 
   echo.channel('bidding')
     .listen('BiddingEvent', (e) => {
-      priceWinner.value = e.bidding.price_winner;
+      harga.start_price.value = e.bidding.price_winner;
     });
 })
 </script>
@@ -49,11 +48,11 @@ onMounted(() => {
   <div class="bg-gray-200 fixed w-11/12 sm:w-4/12 mx-auto left-0 right-0 top-40 index px-8 py-2 border border-black rounded-lg">
     <div v-if="isTawar === 'konfirmasi'" class="my-4">
          <div class="text-center text-2xl font-bold">Konfirmasi Tawaran</div>
-         <div class="text-center text-lg font-bold">Harga sebelumnya : Rp. {{ new Intl.NumberFormat().format(priceWinner) }}</div>
+         <div class="text-center text-lg font-bold">Harga sebelumnya : Rp. {{ new Intl.NumberFormat().format(start_price) }}</div>
          <div class="text-center">Anda akan menaikkan harga lelang saat ini sebesar </div>
          <div class="text-center font-bold">Rp {{ new Intl.NumberFormat().format(price_offer) }}</div>
          <div class="text-center">Penawaran harga Anda akan naik menjadi</div>
-         <div class="text-center font-bold">Rp. {{ new Intl.NumberFormat().format(priceWinner + price_offer)}} </div>
+         <div class="text-center font-bold">Rp. {{ new Intl.NumberFormat().format(start_price + price_offer)}} </div>
          <div class="text-center my-4">Anda Yakin Setuju ?</div>
          <div class="flex flex-row justify-center">
           <button @click="close" class="bg-red-500 text-white border px-4 py-2 rounded-lg w-1/3 border-black mx-4">Tidak</button>
