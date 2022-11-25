caa6b4716e2809bc291f6aa26d150c54be873e5b
Aldi L William <aldyy1988@gmail.com>
2022-11-25 16:26:09 +0700

    Update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -148,15 +148,15 @@
                 <img :src="logo" class="w-32">
               </div>
           </div> 
-          <h1 class="text-center text-3xl font-bold mb-2">SELAMAT DATANG</h1>
-          <p class="text-center mb-2 text-lg">Platform LELANG MENANG Mobil Bekas Berkualitas</p>
+          <h1 class="text-center text-3xl font-bold mb-2 text-white">SELAMAT DATANG</h1>
+          <p class="text-center mb-2 text-lg text-white">Platform LELANG MENANG Mobil Bekas Berkualitas</p>
           <div class="w-full mx-auto">
-              <p class="text-left">Username</p>
+              <p class="text-left text-white">Username</p>
               <input placeholder="Masukan No. WhatsApp" class="bg-white px-2 py-1 rounded border border-black w-full" v-model="dataLogin.no_whatsapp" type="number"/>
           </div>
      
           <div class="w-full mx-auto my-4">
-            <p class="text-left">Password</p>
+            <p class="text-left text-white">Password</p>
             <input placeholder="Masukan Password" class="bg-white px-2 py-1 rounded border border-black w-full" v-model="dataLogin.password" :type="passwordFieldType"/>
             <div class="relative" v-show="isAnimated2">
               <button @click="switchVisibility()" class="absolute -top-[25px] right-4"><img :src="mata_password" class="w-6 h-4"></button>
@@ -167,7 +167,7 @@
           <div class="w-full mx-auto mb-2 flex flex-col sm:flex-row justify-between">
             <div>
               <input type="checkbox"/>
-              <span class="mx-2">Ingat Password Saya</span>
+              <span class="mx-2 text-white">Ingat Password Saya</span>
             </div>   
           </div>
           <button class="bg-blue-500 text-white px-2 py-1 w-full mx-auto rounded" @click="logintodashboard()">Masuk</button>
