1a28fdcbfae8c7bd34b6afca491908a1a6cacc20
Aldi L William <aldyy1988@gmail.com>
2022-11-30 15:47:48 +0700

    Update

--- src/components/CardComponentMobil.vue
+++ src/components/CardComponentMobil.vue
@@ -89,7 +89,7 @@ onMounted(() => {
 </script>
 <template>
   <div>
-    <div class="rounded shadow-2xl p-4 h-full">
+    <div class="rounded p-4 h-full">
     <div class="relative overflow-hidden">
       <div class="absolute bg-blue-500 rounded text-white top-3 left-3 px-2">{{ product.code }}</div>
       <!-- <div class="bg-red-600 absolute bottom-0 right-0 px-4 py-0 rounded-tl-full text-white flex items-start">
